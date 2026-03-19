7d706df41bca08f155e628b0eba5a3a2f970c38f
owen22s <34884@ma-web.nl>
2026-03-19 15:31:12 +0100

    Update InteractAble.cs

--- Zeebeeld Operator/Assets/Scripts/Interaction system/InteractAble.cs
+++ Zeebeeld Operator/Assets/Scripts/Interaction system/InteractAble.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class InteractAble : MonoBehaviour, IInterface
 {
+    /// <summary>
+    /// add a script that is interactable
+    /// </summary>
     public void Interact()
     {
         Debug.Log("InterAction");
