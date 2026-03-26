dd1a193eefc8f218e82ccd8b6a6c607ae258fb6e
Ivo <112894315+IvoBergen@users.noreply.github.com>
2026-03-26 11:02:20 +0100

    Added the Npc Movement

---
+++ Zeebeeld Operator/Assets/Scripts/NPCSystems/NPCRotation.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+/// <summary>
+/// <c>NPCRotation</c> Handles the rotation of the npc.
+/// </summary>
+public class NPCRotation : MonoBehaviour
+{
+    [SerializeField] private float _rotationSpeed = 5f;
+    [SerializeField] private float _alignmentThreshold = 2f; // degrees
+
+    private NPCMovement _npcMovement;
+
+    private void Awake()
+    {
+        _npcMovement = GetComponent<NPCMovement>();
+    }
+
+    private void Update()
+    {
+        if (_npcMovement.State != NPCState.Rotating) return;
+
+        Vector3 direction = (_npcMovement.Target.position - transform.position);
+        direction.y = 0f;
+
+        if (direction.sqrMagnitude < 0.001f) return;
+
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+
+
+        if (Quaternion.Angle(transform.rotation, targetRotation) < _alignmentThreshold)
+            _npcMovement.SetMoving();
+    }
+}
