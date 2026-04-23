cbe8650af37971e9f65076e04e2ed1cdcffa1bdb
owen22s <34884@ma-web.nl>
2026-04-23 11:05:36 +0200

    added materials and changed menu manager

--- ZeebeeldOperator/Assets/Scripts/GameManagerScripts/MenuManager.cs
+++ ZeebeeldOperator/Assets/Scripts/GameManagerScripts/MenuManager.cs
@@ -12,6 +12,7 @@ public class MenuManager : MonoBehaviour
     [Header("Variables")]
     public bool GuessUIActive;
     private bool _pausemenuActive;
+    bool _gameHasStarted = false;
 
     [Header("References")]
     [SerializeField] private GameStateManager _gameStateManager;
@@ -42,6 +43,10 @@ public class MenuManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            if (_gameHasStarted == false)
+            {
+                return;
+            }
             if (GuessUIActive)
             {
                 return;
@@ -67,6 +72,7 @@ public class MenuManager : MonoBehaviour
     {
         _mainMenuHolder.SetActive(false);
         Unpause();
+        _gameHasStarted = true;
     }
 
     /// <summary>
@@ -99,6 +105,7 @@ public class MenuManager : MonoBehaviour
 
         Application.Quit();
     }
+
 
     public void Pause()
     {
